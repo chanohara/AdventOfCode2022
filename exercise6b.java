c4edd27a28f7acdff72bac0ea4a3fdae8c430fc3
Dennis Gauss <chan05@hotmail.de>
2022-12-10 21:43:44 +0100

    Exercise 6a and b

---
+++ exercise6b.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class exercise6b{
+
+    static final int messageLength = 14;
+
+    public static void main( String[] args){
+        exercise6b ex6b = new exercise6b();
+        ex6b.run();
+    }
+
+    public void run(){
+        List<String> fileContent = new ArrayList<String>();
+        try {
+            fileContent = Files.readAllLines(Paths.get("/Users/dennisgauss/Documents/Coding/AdventOfCode2022/ex6_input.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String input = fileContent.get(0);
+        HashSet set = new HashSet();
+        for(int i=0;i<input.length()-messageLength;i++){
+            set.clear();
+            for(int j=0;j<messageLength;j++){
+                set.add(input.charAt(i+j));
+            }
+
+            if(set.size() == messageLength){
+                System.out.println(i+messageLength);
+                return;
+            }
+
+        }
+    }
+
+}
+
